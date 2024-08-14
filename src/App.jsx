2931f0751caf4dd0e0f179bae281214e1adcba33
XIN <60823033+anxndsgn@users.noreply.github.com>
2024-08-14 20:52:21 +0800

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Button, Container, Typography, Box } from '@mui/material';
 
 function App() {
   const [port, setPort] = useState(null);
+  const [connected, setConnected] = useState(false);
+  const [portInfo, setPortInfo] = useState(null);
 
   async function connectToArduino() {
     try {
       const selectedPort = await navigator.serial.requestPort();
       await selectedPort.open({ baudRate: 9600 });
       setPort(selectedPort);
-      console.log('Connected to Arduino');
+      setConnected(true);
+      setPortInfo(await selectedPort.getInfo().usbVendorId);
     } catch (err) {
       console.error('Error:', err);
     }
@@ -17,6 +20,7 @@ function App() {
   async function sendCommand(command) {
     if (!port) {
       console.error('Not connected to Arduino');
+      alert('Not connected to Arduino');
       return;
     }
     const writer = port.writable.getWriter();
@@ -32,15 +36,16 @@ function App() {
           MagnetBase Control Panel
         </Typography>
         <Button variant='contained' onClick={connectToArduino} sx={{ mb: 2 }}>
-          Connect to MagnetBase
+          {connected ? `Connected to ${portInfo}` : `Connect to MagnetBase`}
         </Button>
         <div className='grid grid-cols-3 gap-2'>
           {Array.from({ length: 9 }, (_, i) => (
-            <MagPixel key={i} sendCommand={sendCommand}>
+            <MagPixel key={i} sendCommand={sendCommand} command={i + 1}>
               {i + 1}
             </MagPixel>
           ))}
         </div>
+        <span></span>
       </Box>
     </Container>
   );
@@ -49,9 +54,13 @@ function App() {
 export default App;
 
 // eslint-disable-next-line react/prop-types
-function MagPixel({ sendCommand, children }) {
+function MagPixel({ sendCommand, command, children }) {
   return (
-    <Button variant='outlined' onClick={() => sendCommand()}>
+    <Button
+      variant='outlined'
+      onClick={() => sendCommand(command)}
+      className=' h-24'
+    >
       {children}
     </Button>
   );
